374cb1df9608ebbbf2d151e78f6bf9e4b5ce2e7a
AARON JOHNSON <jardaaroncs@gmail.com>
2023-02-21 13:25:46 -0800

    add event list component with data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,14 +1,12 @@
+import { getAllEvents } from '../../dummydata';
+import EventList from '../../components/events/event-list';
+
 function AllEventsPage() {
-	const events = ['event1', 'event2', 'event3', 'event4', 'event5'];
+	const events = getAllEvents();
 
 	return (
 		<div>
-			<ul>
-				{/* iterate over each event in array */}
-				{events.map((event, index) => (
-					<li key={index}>{event}</li>
-				))}
-			</ul>
+			<EventList items={events} />
 		</div>
 	);
 }
