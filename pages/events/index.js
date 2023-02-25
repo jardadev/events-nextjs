7403a77eb9339e4b14f8f9b9076ad5a3b95011a3
AARON JOHNSON <jardaaroncs@gmail.com>
2023-02-25 01:44:05 -0800

    fix add EventSearch component import

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,6 +1,7 @@
 import { getAllEvents } from '../../dummydata';
 import { useRouter } from 'next/router';
 import EventList from '../../components/events/event-list';
+import EventSearch from '../../components/events/event-search';
 
 function AllEventsPage() {
 	const events = getAllEvents();
