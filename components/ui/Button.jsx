9150a41331c58117d02d60f958caf042f98dcb42
AARON JOHNSON <jardaaroncs@gmail.com>
2023-02-25 01:00:17 -0800

    add general use functionality to component

--- components/ui/Button.jsx
+++ components/ui/Button.jsx
@@ -2,11 +2,21 @@ import Link from 'next/link';
 import classes from './Button.module.css';
 
 function Button(props) {
+	const { link, onClick, children } = props;
+
+	// if link prop is defined, render a link button else render a normal button
+	if (link) {
+		return (
+			<Link className={classes.btn} href={props.link}>
+				{children}
+			</Link>
+		);
+	}
+
 	return (
-		<Link className={classes.btn} href={props.link}>
+		<button onClick={onClick} className={classes.btn}>
 			{props.children}
-		</Link>
+		</button>
 	);
 }
-
 export default Button;
